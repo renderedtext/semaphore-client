70205c93d4e7f638a6827d03cdf4ff9243a6b69d
Semaphore Robot <semaphore.robot@gmail.com>
2017-08-07 14:41:45 +0000

    Auto-generated commit

---
+++ scripts/versions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOCAL_VERSION=$(cat lib/semaphore_client/version.rb | grep VERSION | cut -d " " -f 5 | tr -d '""')
+REMOTE_VERSION=$(fury list --as=renderedtext | grep "semaphore_client" | cut -d " " -f 2 | tr -d '()')
+
+if [ $LOCAL_VERSION = $REMOTE_VERSION ]; then
+  echo "Version ${LOCAL_VERSION} already exists.";
+  exit 1;
+fi
